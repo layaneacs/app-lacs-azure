e61c8a74abc6e66492f7cba2ba71ee870d34e378
layane.andreia@hotmail.com <layane.andreia@hotmail.com>
2020-08-15 23:32:29 -0300

    página inicial - front

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+[Area("Home")]
+public class HomeController : Controller
+{
+    
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,7 @@ namespace com.lacs.azure
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +27,14 @@ namespace com.lacs.azure
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
+            app.UseRouting();          
+           
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();              
             });
         }
     }
